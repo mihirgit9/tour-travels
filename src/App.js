009821325a3ddcf3851eef2a5f4095e1f5838c12
mihirgit9 <mihirmoofy@gmail.com>
2023-04-11 01:48:10 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
+import { useState } from 'react';
+import Card from './components/Card';
 
 function App() {
+  const [tours, setTour] = useState(data);
+
+  function hnadleRefresh(){
+    setTour(data);
+  }
+
+  
+  if(tours.length===0){
+    return(
+      <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>
+        <p className='font-bold text-2xl text-red-600'>OOPS!</p>
+        <div className='text-xl'>No More Cities Left</div>
+        <button onClick={hnadleRefresh} className='my-4 px-3 border-purple-600 border-solid border-2 rounded-md bg-purple-600 text-white select-none hover:bg-white hover:text-black'>Refresh</button>
+      </div>
+    ); 
+  };
+    
+  
+  
+  function removeCity(id){
+    let newTours=[];
+    tours.forEach((tour)=>{
+      if(tour.id!==id){
+        newTours.push(tour);
+      }
+    });
+    setTour(newTours);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className=' text-red-600 text-center font-bold text-3xl py-4 px-6 border-dotted border-blue-600 border-4 rounded-lg w-[20rem] mx-auto my-4 '>
+      Tour and Travels</h1>
+      <div className=' flex flex-wrap gap-4 justify-center w-[1000px] mx-auto my-8'>
+        {
+          tours.map((tour)=>{
+            return(
+              <Card key={tour.id} city={tour} removeCity={removeCity}></Card>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function Card(props){
+    let tour=props.city;
+    let func = props.removeCity;
+    const [readmore, setReadmore]=useState(true);
+    let showmore = readmore ? "showmore" : "showless";
+    let detail= `${tour.info.substring(0,200)}... `;
+    let description = readmore ? detail : tour.info;
+
+    function handleDesc(){
+        setReadmore(!readmore);
+    }
+
+    return(
+        <div className=" w-[30%] flex flex-col">
+            <img src={tour.image} alt="img" className=" w-[100%] h-[250px] rounded-md hover:scale-[1.02] transition-all"></img>
+            <p className=" mt-2 text-green-600 font-bold">₹ {tour.price}</p>
+            <p className=" text-xl font-bold font-serif">{tour.name}</p>
+            <div className=" px-2 text-justify text-sm">
+                {description}
+                <span onClick={handleDesc} className=" text-blue-500 cursor-pointer font-bold">
+                {showmore}</span>
+            </div>
+            <button onClick={()=>{
+                func(tour.id);
+            }} className=" my-4 px-3 border-purple-600 border-solid border-2 rounded-md select-none hover:bg-purple-600 hover:text-white">
+            Not Interested</button>
+        </div>
+    );
+}
+
+export default Card;
